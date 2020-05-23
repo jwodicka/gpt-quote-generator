7eb4d7d869c8b3b83fbd816667b4525c0ec63e69
Jason Wodicka <jason@wodicka.net>
2020-05-23 14:40:24 -0700

    Increase the number of command-line options

--- demo.py
+++ demo.py
@@ -20,15 +20,20 @@ def main(
         resume: ('Resume training from a prior run?', 'flag'),
 
         # Options
-        # There are multiple GPT-2 models that have been released. 124M is the smallest, and it gives more than adequate results.
+        # There are multiple GPT-2 models that have been released. 124M is the smallest, and it
+        # gives more than adequate results.
         model_name: ('Name of the GPT-2 model to use', 'option')='124M',
         run_name: ('Name to give this run - used for resuming prior runs', 'option')='run1',
         steps: ('Number of steps of training to carry out', 'option', None, int)=100,
         nsamples: ('Number of generation passes to run', 'option', None, int)=1,
+        save_every: ('Save a checkpoint every this many steps', 'option', None, int)=200,
+        output_file: ('Name of the csv file to write', 'option')=None,
     ):
 
-    model_directory = 'models' # If we want, we could make this configurable, but there's some testing involved to make
-                               # sure we do so consistently everywhere.
+    model_directory = 'models' # If we want, we could make this configurable, but there's some
+                               # testing involved to make sure we do so consistently everywhere.
+    temporary_input_file = 'temp_input.csv' # The file containing cleaned data from the quotes
+
 
     print(f"Using model: {model_name}")
 
@@ -45,8 +50,8 @@ def main(
     attributions = Counter()
     # Track all individual quotes so that we can check whether we've regenerated them.
     source_quotes = []
-    with open('quotes.20200503.csv', newline='') as quote_file:
-        with open('barequotes.csv', 'w', newline='') as out_file:
+    with open(source, newline='') as quote_file:
+        with open(temporary_input_file, 'w', newline='') as out_file:
             quote_reader = csv.DictReader(quote_file)
             quote_writer = csv.DictWriter(out_file, fieldnames=['quote'], extrasaction='ignore')
             quote_writer.writeheader() # Loader assumes there will be a header and skips it.
@@ -58,40 +63,50 @@ def main(
     print("Loaded {} quotes attributed to {} sources.".format(len(source_quotes), len(attributions)))
     print("Top 10 sources:")
     print(attributions.most_common(10))
-    # print(attributions)
-
-    file_name="barequotes.csv"
 
     sess = gpt2.start_tf_sess()
 
-    # Original training was performed on 1000 steps.
-    # At step 1000 we were frequently reiterating lines in the source.
-    # At step 400 we were sometimes recycling existing quotes.
-
     if resume:
         print(f"Loading run {run_name}")
+        # If the model name is set, the run name will be ignored.
         gpt2.load_gpt2(sess, run_name=run_name)
 
 
     if finetune:
         print('Fine-tuning the model from training data')
-        gpt2.finetune(sess, file_name, model_name=model_name, steps=steps, run_name=run_name)
+        gpt2.finetune(
+            sess,
+            temporary_input_file,
+            model_name=model_name,
+            steps=steps,
+            run_name=run_name,
+            save_every=save_every,
+            max_checkpoints=100, # How many checkpoints to keep for each run
+        )
 
 
     print('Generating quotes')
-    quotes_with_delimiters = gpt2.generate(sess, run_name=run_name, nsamples=nsamples, return_as_list=True)
-    generated_quotes = []
+    quotes_with_delimiters = gpt2.generate(
+        sess,
+        run_name=run_name,
+        nsamples=nsamples,
+        return_as_list=True
+    )
 
-    print('Checking for plagiarism')
+    print('Parsing quotes')
+    generated_quotes = []
     for sample in quotes_with_delimiters:
         print("SAMPLE: [" + sample + "]")
         generated_quotes.extend(find_quotes(sample))
 
+    print('Checking for plagiarism')
     results = []
     for quote in generated_quotes:
-        print("QUOTE: [" + quote + "]")
         closest_match = process.extractOne(quote, source_quotes)
-        # print(closest_match)
+        print("QUOTE: [" + quote + "]")
+        if closest_match[1] >= 90:
+            # This is a bit too close.
+            print("MATCH: [" + closest_match[0] + "]")
         result = {
             "quote": quote,
             "best_match": closest_match[0],
@@ -100,7 +115,11 @@ def main(
         results.append(result)
 
     novel_quote_count = 0
-    with open('generated_quotes.v3.csv', 'w', newline='') as out_file:
+
+    if output_file == None:
+        output_file = run_name + '_' + source
+
+    with open(output_file, 'w', newline='') as out_file:
         quote_writer = csv.DictWriter(out_file, fieldnames=['quote', 'best_match', 'match_score'])
         quote_writer.writeheader()
         for row in results:
